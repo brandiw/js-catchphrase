4421855fe80da8dbfd412e438cf8582ccc2af9f4
brandiw <brandimichelle.0911@gmail.com>
2017-07-27 12:19:21 -0700

    Adding PORT ENV variable

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var flash = require('connect-flash');
 var db = require('./models');
 var router = require('./controllers/game');
 var app = express();
-var io = require('socket.io').listen(app.listen(3000));
+var io = require('socket.io').listen(app.listen(process.env.PORT || 3000));
 
 //Middleware & Stuff
 app.use(session({
